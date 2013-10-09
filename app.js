f50d1c5b337f49c63b083053d280c3e0a61cf097
Taylor Lapeyre <taylorlapeyre@gmail.com>
2013-10-09 12:32:46 -0500

    move javascript to its own file, go crazy math up in here

---
+++ app.js
@@ -0,0 +1,30 @@
+App = function() {
+	this.setOptionBar = function() {
+		var options = $('.option-bar li');
+		var optionWidth = 100 / options.length
+		console.log((-1/160 * Math.pow(options.length - 1, 2) + 1) + "rem");
+
+		// font size is parabola, vertex (1, 1) downward sloping
+		options.css({
+			"width":     optionWidth - 0.4 + "%",
+			"font-size": (-1/150 * Math.pow(options.length - 1, 2) + 1) + "rem"
+		});
+	};
+
+	this.setHoverView = function() {
+		$('.hover-view').hover(function() {
+			$(this).children('.hover-img').css({opacity: 0});
+			$(this).children('.hover-content').css({opacity: 1});
+			$(this).children('.hover-tools').css({opacity: 1});
+		}, function() {
+			$(this).children('.hover-img').css({opacity: 1});
+			$(this).children('.hover-content').css({opacity: 0});
+			$(this).children('.hover-tools').css({opacity: 0});
+		});
+	};
+
+	this.setOptionBar();
+	this.setHoverView();
+};
+
+$(function() { var app = new App() });
